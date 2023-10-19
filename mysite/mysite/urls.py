01d67de90b5672855db3393549ae6569255c1dc4
RoflSQ <teamplaing5@gmail.com>
2023-10-19 10:31:47 +0700

    Нихера не работает

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('school.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
-
-urlpatterns += staticfiles_urlpatterns()
